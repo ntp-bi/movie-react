797d39abcef771ad923e0da16cda2a55c1497c79
ntp-bi <bintp22122002@gmail.com>
2024-01-26 14:49:15 +0700

    make category

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import { useParams } from "react-router";
+
+import PageHeader from "../components/page-header/PageHeader";
+
+import { category as cate } from "../api/tmdbApi";
 
 const Catalog = () => {
-    return (
-        <div>
-            Catalog
-        </div>
-    )
-}
+    const { category } = useParams();
 
-export default Catalog
+    return (
+        <>
+            <PageHeader>
+                {category === cate.movie ? "Movie" : "TV Series"}
+            </PageHeader>
+        </>
+    );
+};
+
+export default Catalog;
